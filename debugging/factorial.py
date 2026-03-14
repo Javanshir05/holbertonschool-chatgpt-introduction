7c1b56be6958bb29fd319d155ef9b25aa93a537d
root <jhashimov17871@ada.edu.az>
2026-03-14 17:25:11 +0000

    Done

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -10,3 +10,15 @@ def factorial(n):
 
 f = factorial(int(sys.argv[1]))
 print(f)
+
+
+# import sys
+
+# def factorial(n):
+#     result = 1
+#     while n > 1:
+#         result *= n
+#     return result
+
+# f = factorial(int(sys.argv[1]))
+# print(f)
---
+++ debugging/print_arguments.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
+
+
+# import sys
+
+# for i in range(len(sys.argv)):
+#     print(sys.argv[i])
